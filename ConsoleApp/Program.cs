2e73820ae404e68c0a99be3a657c1e6bc0d68705
Phil Wright <phil.wright@evenglide.com.au>
2019-04-15 17:16:26 +1000

    Change testing

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             PdfDocument document = new PdfDocument();
-            document.Load(@"d:\Slides.pdf", true);
+            document.Load(@"d:\PDF17.pdf", false);
             document.Close();
 
             PdfCatalog catalog = document.Catalog;
@@ -17,7 +17,7 @@ namespace ConsoleApp
             PdfPagesTree pages = catalog.Pages;
             Console.WriteLine(pages.Count);
             for (int i = 0; i < pages.Count; i++)
-                Console.WriteLine(pages[i].Resources.ToString());
+                Console.WriteLine(pages[i].ToString());
 
             Console.ReadLine();
         }
